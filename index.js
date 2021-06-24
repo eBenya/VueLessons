1bdb5f8c4427929ff1d7152019b52804faf3a772
eBenya <koly_07@mail.ru>
2021-06-24 11:09:16 +0300

    3Part

--- index.js
+++ index.js
@@ -1,109 +1,52 @@
 var app = new Vue({
     el: '#app',
     data: {
-        showPar: false,
-        text: 'someText',
-        message: 'hello',
-        name: 'kek',
-        surname: 'puc',
-        var1: 1,
-        var2: 2,
-        num: 5,
-        num1: 1,
-        num2: 2,
-        num3: 3,
-        numArr: [1, 2, 3],
-        items: ['a', 'b', 'c', 'd', 'e'],
-        users: {
-            employee1: '100$',
-            employee2: '200$',
-            employee3: '300$',
-        },
-        hrefs: [
-            { href: '1.html', text: 'ссылка 1' },
-            { href: '2.html', text: 'ссылка 2' },
-            { href: '3.html', text: 'ссылка 3' },
-        ],
-        products: [
-            { name: 'product1', price: 100, quantity: 5 },
-            { name: 'product2', price: 200, quantity: 4 },
-            { name: 'product3', price: 300, quantity: 3 },
-        ],
-        vorkNum: null,
-        resultSqrNum: 0,
-        isFirstInput: true,
-        input1: '',
-        input2: '',
-        textAreaWordsArray: [],
-        textAreaLongString: '',
-        radioType: '',
-        selected: '',
-        isDisabled: false,
+        someText: 'some text',
+        someTextWithTags: '<a v-on:click.prevent="showText" href="#">SomeLinkTagText</a>',
+        name: '',
+        surname: '',
+        patronymic: '',
+        someNumber: 0,
+        textIsShow: true,
+        buttonInfo: 'kek',
     },
     methods: {
-        executeAllert(mess) {
-            alert(mess);
+        showText: function () {
+            this.buttonInfo = 'kek';
+            this.textIsShow = !this.textIsShow;
         },
-        sayHello: function () {
-            alert(this.message);
+        onClickEventHandler: () => {
+            alert('You tap to the SomeButton');
         },
-        getSumm: function (arr) {
-            let sum = arr.reduce((sum, it) => sum += it);
-            alert(sum);
+        changeText: function (text) {
+            this.someText = text;
         },
-        changeMessage() {
-            this.message = 'Bye';
+        testEvent: (e) => {
+            console.log(e);
+            alert('You tap to ' + e.target.firstChild.data);
         },
-        addItem() {
-            this.items.push('newItem' + this.items.length);
+        onClickKeyBoardHandler: function (event) {
+            this.buttonInfo = `key code - ${event.which}; key - ${event.key}`;
+            //this.forceUpdate();
         },
-        removeItem(index) {
-            this.items.splice(index, 1);
+        onClickMouseHandler: function(event) {
+            let mouseButton = '';
+            if (event.which === 1)
+                mouseButton = 'LBM';
+            else if (event.which === 2)
+                mouseButton = 'RBM';
+            else if (event.which === 3)
+                mouseButton = 'MBM';
+
+            this.buttonInfo = `key code - ${event.which}; key - ${mouseButton}`;
+        }
+    },
+    computed: {
+        fullName: function () {
+            return `${this.surname} ${this.name} ${this.patronymic}`;
         },
-        reverseItems() {
-            this.items.reverse();
-        },
-        sortItems() {
-            this.items.sort();
-        },
-        filterFirst10() {
-            this.items = this.items.filter((o, key) => key > 0 && key < 10);
-        },
-        //str.charAt(0).toUpperCase()+str.slice(1, str.length-1)+str.charAt(str.length-1).toUpperCase();
-        upperCaseFirst() {
-            this.text = this.text.charAt(0).toUpperCase() + this.text.slice(1);
-        },
-        upperCaseLast() {
-            this.text = this.text.slice(0, this.text.length - 1) + this.text.charAt(this.text.length - 1).toUpperCase()
-        },
-        upperCaseFirstAndLast() {
-            this.upperCaseFirst();
-            this.upperCaseLast();
-        },
-        squareItem(index) {
-            this.numArr = this.numArr.map((o, i) => i === index ? o ** 2 : o);
-        },
-        showToogleMode() {
-            this.showPar = !this.showPar;
-        },
-        calcSqrt() {
-            this.resultSqrNum = Math.sqrt(this.vorkNum);
-        },
-        switchInput(isFirst) {
-            this.isFirstInput = isFirst;
-        },
-        confirmInputChange() {
-            if (this.isFirstInput) {
-                this.input2 = this.input1;
-            }
-            else {
-                this.input1 = this.input2;
-            }
-        },
-        createWordArray() {
-            if (this.textAreaLongString) {
-                this.textAreaWordsArray = this.textAreaLongString.split(' ').map(x => x);
-            }
-        },
-    }
+        sqrSomeNumber: function () {
+            return this.someNumber ** 2;
+        }
+    },
 });
